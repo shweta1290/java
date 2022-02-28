340d4f28a0a735f1ffde0ee7f6ec2323d2c20dce
tripa <tripa@LAPTOP-8R8RV7IG>
2022-02-28 19:11:23 +0530

    1st commit

---
+++ Hello/src/day0/Table.java
@@ -0,0 +1,15 @@
+package day0;
+
+public class Table {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+		for(int i=0;i<=10;i++)
+		{
+			
+			System.out.println("5"+"*"+i+"="+(5*i));
+			
+		}
+	}
+
+}

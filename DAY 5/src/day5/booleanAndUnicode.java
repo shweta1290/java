d02ff0c99bc64b769e034162a3930da76c6a4cea
tripa <tripa@LAPTOP-8R8RV7IG>
2022-03-01 18:47:31 +0530

    day5 booleanAndunicode

---
+++ DAY 5/src/day5/booleanAndUnicode.java
@@ -0,0 +1,17 @@
+package day5;
+
+public class booleanAndUnicode {
+	public static void main(String[] args) {
+		boolean var = false;
+		
+		System.out.println(var);
+		var=!(var);
+		System.out.println(var);
+
+		char var1 = '\u0060';
+		System.out.println(var1);
+		
+	}
+
+
+}

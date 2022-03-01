8adfee25c53eff78b2568b2ca8369e9350a9948f
tripa <tripa@LAPTOP-8R8RV7IG>
2022-03-01 19:01:11 +0530

    day5 typecasting

---
+++ DAY 5/src/day5/typecaste.java
@@ -0,0 +1,12 @@
+package day5;
+
+public class typecaste {
+	public static void main(String[] args) {
+		
+	       float f = 10.532f;
+	        long l =(long) f;
+	        System.out.println((double)f);
+	       System.out.println(l);
+		}
+
+}

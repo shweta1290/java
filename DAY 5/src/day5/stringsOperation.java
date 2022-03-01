a1a4c62fa8ff978f92e4822a67eda2f4957e2901
tripa <tripa@LAPTOP-8R8RV7IG>
2022-03-01 18:56:27 +0530

    day5 strings

---
+++ DAY 5/src/day5/stringsOperation.java
@@ -0,0 +1,21 @@
+package day5;
+
+public class stringsOperation {
+	public static void main(String[] args) {
+		String var = new String("Hello world");
+		System.out.println(var);
+		String s="shweta" ;
+		System.out.println(s);
+		String s1="Welcome";  
+		String s2="Welcome";//It doesn't create a new instance  
+        char ch=s1.charAt(4);
+        System.out.println(ch);
+        System.out.println(s1.length());
+        char[] c={'j','a','v','a','t','p','o','i','n','t'};  
+        String x=new String(c); 
+        
+		
+	}
+
+
+}

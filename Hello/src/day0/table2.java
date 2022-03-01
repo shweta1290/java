32fe1716445aad76e98a52a533699c84d51d5dcb
tripa <tripa@LAPTOP-8R8RV7IG>
2022-03-01 16:45:33 +0530

    commit table

---
+++ Hello/src/day0/table2.java
@@ -0,0 +1,15 @@
+package day0;
+
+public class table2 {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+		for(int i=0;i<=10;i++)
+		{
+			
+			System.out.println("5"+"*"+i+"="+(5*i));
+			
+		}
+	}
+
+}

feee6eb46cacf9d2090c381cc7f297e8ea00854c
tripa <tripa@LAPTOP-8R8RV7IG>
2022-03-10 10:33:48 +0530

    hello world

---
+++ Hello/src/day0/helloworld.java
@@ -0,0 +1,9 @@
+package day0;
+
+public class helloworld {
+	public static void main(String args[]) {
+	System.out.println("hello world");
+	System.out.println("hello world".charAt(6));
+	System.out.println("hello world".compareTo("Hello world"));
+
+}}
